b37b893f784174e674b22a87f4007c6f9abdc6e2
Alexsey Loginov <aleniklog@gmail.com>
2022-07-15 00:07:43 +0300

    no message

---
+++ VKclient/Controllers/NewsVC/NewsVC.swift
@@ -0,0 +1,17 @@
+//
+//  NewsVC.swift
+//  VKclient
+//
+//  Created by Алексей Логинов on 11.07.2022.
+//
+
+import UIKit
+
+class NewsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
